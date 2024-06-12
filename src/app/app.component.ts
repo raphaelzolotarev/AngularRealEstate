3ea6a9eb2f8a5572d61c2fdd7072679ea2db218d
raphaelzolotarev <raphaelzolotarev@gmail.com>
2024-06-12 14:47:58 +0200

    PAGE DETAILS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../app/home/home.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HomeComponent],
+  template: '<main> <header class="brand-name"> <img class="brand-logo" width="200px" src="/assets/logo.svg" alt="logo bmw" aria-hidden="true"> </header> <section class="content"> <app-home></app-home> </section> </main>',
+  styleUrl: './app.component.scss', 
 })
 export class AppComponent {
   title = 'home';
