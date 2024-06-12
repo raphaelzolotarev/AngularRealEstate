459d620a8d83ee0c24449fe62255cbe2726461ba
raphaelzolotarev <raphaelzolotarev@gmail.com>
2024-06-12 14:55:30 +0200

    page details and lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../app/home/home.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  template: '<main> <header class="brand-name"> <img class="brand-logo" width="200px" src="/assets/logo.svg" alt="logo bmw" aria-hidden="true"> </header> <section class="content"> <app-home></app-home> </section> </main>',
+  imports: [RouterModule, HomeComponent],
+  template: `
+    <main> 
+      <header class="brand-name"> 
+        <img class="brand-logo" width="200px" src="/assets/logo.svg" alt="logo bmw" aria-hidden="true"> 
+      </header> 
+      <section class="content"> 
+        <router-outlet></router-outlet> 
+      </section> 
+    </main>
+    `,
   styleUrl: './app.component.scss', 
 })
 export class AppComponent {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -22,116 +23,12 @@ import { HousingLocation } from '../housing-location';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  housingLocationList:HousingLocation[]=[
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: '/assets/1.jpeg',
-      availableUnits: 4,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: '/assets/2.jpg',
-      availableUnits: 0,
-      wifi: false,
-      laundry: true
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: '/assets/3.jpeg',
-      availableUnits: 1,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: '/assets/4.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
-      photo: '/assets/5.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/6.webp',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/7.webp',
-      availableUnits: 5,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/8.webp',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/9.jpg',
-      availableUnits: 10,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
-      photo: '/assets/10.webp',
-      availableUnits: 6,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 10,
-      name: 'Centraal Station',
-      city: 'Brussel',
-      state: 'BE',
-      photo: '/assets/11.jpg',
-      availableUnits: 6,
-      wifi: true,
-      laundry: true
-    }
-  ];
+
+  housingLocationList:HousingLocation[]=[];
+  housingService:HousingService=inject(HousingService);
+
+  constructor(){
+    this.housingLocationList = this.housingService.getAllHousingLocation();
+  }
+
 }
